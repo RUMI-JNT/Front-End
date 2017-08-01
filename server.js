ce7baedf3c51949b1f86ebce0887ae511dd00aa7
charlifrank <jayfrank0528@gmail.com>
2017-08-01 00:32:43 -0500

    (feat) Create public folder to serve static files

--- server.js
+++ server.js
@@ -7,14 +7,4 @@ app.listen(8080, () => {
   console.log("app listening on port 8080")
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(`${__dirname}/index.html`));
-})
-
-app.get('/sharedobject', (req, res) => {
-  res.sendFile(path.resolve(`${__dirname}/sharedobject-info.html`));
-})
-
-app.get('/shareddrive', (req, res) => {
-  res.sendFile(path.resolve(`${__dirname}/shareddrive.html`));
-})
+app.use(express.static('public'));
